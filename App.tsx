ea39804df7aeb602d02e554690f55b4b2f171257
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-05-07 05:23:06 +0200

    Add Main.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/components/Main";
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const App = () => <Main />;
 
 export default App;
