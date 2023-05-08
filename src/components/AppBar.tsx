ed5cd332bd51db9d00b68e9bcac781278d9cb189
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-05-08 16:16:16 +0200

    Add horizontal scroll view to AppBar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { Link } from "react-router-native";
 
 import Constants from "expo-constants";
@@ -8,34 +8,28 @@ import theme from "../theme";
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    flexDirection: "row",
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.color.background,
   },
-
   pressable: {
     padding: 20,
-  },
-
-  signIn: {
-    marginLeft: "auto",
-    marginRight: 0,
   },
 });
 
 const AppBar = () => (
   <View style={styles.container}>
-    <Link to="/" style={[styles.pressable]}>
-      <Text fontWeight="bold" fontSize="subheading" color="textWhite">
-        Repositories
-      </Text>
-    </Link>
-    <Link to="/sign-in" style={[styles.pressable, styles.signIn]}>
-      <Text fontWeight="bold" fontSize="subheading" color="textWhite">
-        Sign in
-      </Text>
-    </Link>
+    <ScrollView horizontal>
+      <Link to="/" style={[styles.pressable]}>
+        <Text fontWeight="bold" fontSize="subheading" color="textWhite">
+          Repositories
+        </Text>
+      </Link>
+      <Link to="/sign-in" style={styles.pressable}>
+        <Text fontWeight="bold" fontSize="subheading" color="textWhite">
+          Sign in
+        </Text>
+      </Link>
+    </ScrollView>
   </View>
 );
 
